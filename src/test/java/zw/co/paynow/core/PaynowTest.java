ed0a43671ff785ce21cf563f21b89c6046373374
Adrian Maenzanise <adrian@paynow.co.zw>
2025-01-23 12:16:00 +0200

    Replace deprecated mockito runner import

--- src/test/java/zw/co/paynow/core/PaynowTest.java
+++ src/test/java/zw/co/paynow/core/PaynowTest.java
@@ -5,7 +5,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import zw.co.paynow.exceptions.ConnectionException;
 import zw.co.paynow.exceptions.HashMismatchException;
 import zw.co.paynow.http.HttpClient;
